9374f3234e8fac1ab229e94838561e333a1b2f72
tomdaniels <thomsondaniels@hotmail.com>
2018-08-11 19:46:15 +1000

    styles: general background/header etc

---
+++ src/js/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+  <div className="scratchpad__header-wrap">
+      <div className="scratchpad__header-item">
+          {'scratch.tomd.io | online javascript scratchpad'}
+      </div>
+  </div>
+);
+
+export default Header;
