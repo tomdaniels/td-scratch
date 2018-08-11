af0fe1f393a9378ae13428400d28cd397f8c85d7
tomdaniels <thomsondaniels@hotmail.com>
2018-08-11 21:36:35 +1000

    test: landing page tests

--- src/js/components/landing.js
+++ src/js/components/landing.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { UnControlled as CodeMirror } from 'react-codemirror2';
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/theme/material.css';
 import Header from './header';
 
 
--- test/components/landing.js
+++ test/components/landing.js
@@ -1,9 +1,19 @@
 import Landing from '../../src/js/components/landing';
 
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<Landing />);
+});
+
 describe('landing.js', () => {
   describe('<Landing />', () => {
-    it('has no tests', () => {
-      // See Enzyme documentation: https://github.com/airbnb/enzyme
+    it('renders the page', () => {
+      expect(wrapper).to.be.present();
     });
+
+    it('renders the header', () => {
+      expect(wrapper.find('Header')).to.be.present();
+    })
   });
 });
